921038bf297eb0e3e0dbdfde3b026085ec248f0f
Bryce <95647820+bryce-24@users.noreply.github.com>
2025-03-20 09:33:42 -0700

    fixed loading screen

--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,13 @@
-let isClicked = false;
-let gameLoopRunning = false;
-let score = 0;
 
-// Function to show the loading message
+// show loading message
 function showLoading() {
     const username = document.getElementById('username').value;
-    const loadingText = document.getElementById('loading-text');
-    loadingText.textContent = `Retrieving film data for ${username}...`;
+    const loadingTextA = document.getElementById('loading-text-a');
+    const loadingTextB = document.getElementById('loading-text-b');
+    loadingTextA.textContent = `retrieving film data for ${username}...`;
+    loadingTextB.textContent = `(this may take a few minutes)`;
     document.getElementById('loading').style.display = 'block';
 
-    // Hide elements with the class 'hide-on-submit'
     const elementsToHide = document.querySelectorAll('.hide-on-submit');
     elementsToHide.forEach(element => {
         element.style.display = 'none';
@@ -19,7 +17,7 @@ function showLoading() {
     }
 }
 
-// Function to hide the loading message initially
+// hide loading message initially
 function hideLoading() {
     document.getElementById('loading').style.display = 'none';
 }
@@ -33,50 +31,3 @@ document.addEventListener('DOMContentLoaded', function() {
         showLoading();
     });
 });
-
-function startLoadingGame() {
-    const orangeCircle = document.getElementById('orangeCircle');
-    const greenCircle = document.getElementById('greenCircle');
-    const blueCircle = document.getElementById('blueCircle');
-
-    orangeCircle.addEventListener('click', () => {
-        score++;
-        console.log('orange circle clicked.');
-        closeCircle(orangeCircle);
-    });
-    
-    greenCircle.addEventListener('click', () => {
-        score++;
-        console.log('green circle clicked.');
-        closeCircle(greenCircle);
-    });
-    
-    blueCircle.addEventListener('click', () => {
-        score++;
-        console.log('blue circle clicked.');
-        closeCircle(blueCircle);
-    });
-
-    function gameLoop() {
-        openCircle(orangeCircle);
-        openCircle(greenCircle);
-        openCircle(blueCircle);
-        setTimeout(() => {
-            closeCircle(orangeCircle);
-            closeCircle(greenCircle);
-            closeCircle(blueCircle);
-        }, 30000000);
-    }
-    gameLoopRunning = true;
-    gameLoop();
-}
-
-
-
-function openCircle(circle) {
-    circle.style.display = 'block';
-}
-
-function closeCircle(circle) {
-    circle.style.display = 'none';
-}
